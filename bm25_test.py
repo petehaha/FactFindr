e532ebe895b56a406f6806b9affb0b3e9da48073
Jasmine Gobbo <jazzgobbo@dhcp-10-105-43-144.wireless.northwestern.private>
2022-03-08 11:14:20 -0600

    scoring passages from articles using bm25

--- bm25_test.py
+++ bm25_test.py
@@ -6,8 +6,19 @@ passage_list = [
     "How is the weather today?"
 ]
 
+search_query = "windy London"
 def bm25_passage(passage_list, search_query):
     tokenized_passage = [doc.split(" ") for doc in passage_list]
 
     bm25 = BM25Okapi(tokenized_passage)
     # <rank_bm25.BM25Okapi at 0x1047881d0>
+
+    tokenized_query = search_query.split(" ")
+
+    doc_scores = bm25.get_scores(tokenized_query)
+    # array([0.        , 0.93729472, 0.        ])
+
+    return bm25.get_top_n(tokenized_query, passage_list, n=1)
+    # ['It is quite windy in London']
+
+print(bm25_passage(passage_list, search_query))
